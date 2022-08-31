c51ae910baed40b1b49283878d0cda19473f6148
thanawis <thana.wisutphusi@gmail.com>
2022-08-31 21:40:14 +0700

    test api/hello

--- client/pages/api/hello.ts
+++ client/pages/api/hello.ts
@@ -2,13 +2,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 
-type Data = {
-  name: string;
-};
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.status(200).json({ name: "john doe" });
+  const { data } = await axios.get("http://backend:1337/api/users");
+
+  res.status(200).json({ data });
 }
