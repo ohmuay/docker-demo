bfdfb764990fd6294436da8519c82e968f9c67fa
thanawis <thana.wisutphusi@gmail.com>
2022-08-31 20:24:10 +0700

    init

---
+++ client/pages/api/hello.ts
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+
+type Data = {
+  name: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  res.status(200).json({ name: "john doe" });
+}
